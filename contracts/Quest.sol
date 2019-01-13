7b7b4ef75b26fbd835a57afad50833eb6c197c6d
Ian lapham <ianlapham@gmail.com>
2019-01-13 16:22:29 -0500

    updated quest structure

--- contracts/Quest.sol
+++ contracts/Quest.sol
@@ -2,6 +2,14 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract Quest {
 
+  struct Quest {
+    address[] tokensRequired;
+    bool openForSubmission;
+    address prizeTokenAddress;
+    uint prizeTokenId;
+    address makerAddress;
+  }
+
   constructor() public{
 
   }
